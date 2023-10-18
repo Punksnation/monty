908687890b3e0a537a4a6b8abf597f2b511e89d0
Punksnation <cyberpunk26@gmail.com>
2023-10-18 01:38:15 -0700

    0. push, pall

---
+++ _push.c
@@ -0,0 +1,54 @@
+#include "monty.h"
+
+/**
+ * isNum - checks if a string is a number
+ * @str: string to check
+ * Return: True(1), False(0)
+ */
+int isNum(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] == '-')
+		{
+			if (str[i + 1] == '\0')
+				return (0);
+		}
+		else if (str[i] < 48 || str[i] > 57)
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * stack_push - adds an element to the top of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of the opcode
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node;
+
+	(void)line_number;
+	if (mem.n == NULL || isNum(mem.n) == 0)
+	{
+		free_all(*stack);
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+	{
+		free_all(*stack);
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	node->n = atoi(mem.n);
+	node->prev = NULL;
+	node->next = *stack;
+	if (*stack)
+		(*stack)->prev = node;
+	*stack = node;
+}
---
+++ execute_script.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+/**
+ * execute_script - reads instructions from bytecod file and executes them
+ */
+void execute_script(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	int i;
+	unsigned int line_number = 0;
+	instruction_t ops[] = {
+		{"push", _push}, {"pall", _pall}, {"pint", _pint},
+		{"pop", _pop}, {"swap", _swap}, {"add", _add},
+		{"nop", _nop}, {"sub", _sub}, {"div", _div},
+		{"mul", _mul}, {"mod", _mod}, {"pchar", _pchar},
+		{"pstr", _pstr}, {"rotl", _rotl}, {"rotr", _rotr},
+		{NULL, NULL}
+	};
+	stack_t *stack = NULL;
+
+	while (getline(&line, &len, mem.pScript) != -1)
+	{
+		line_number++;
+		/* Finds opcode in ops[] */
+		mem.token = strtok(line, " \n");
+		if (mem.token == NULL || mem.token[0] == '#')
+			continue;
+		for (i = 0; ops[i].opcode != NULL; i++)
+		{
+			if (strcmp(ops[i].opcode, mem.token) == 0)
+			{
+				mem.n = strtok(NULL, " \n");
+				ops[i].f(&stack, line_number);
+				break;
+			}
+		}
+		if (ops[i].opcode == NULL)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, mem.token);
+			free_all(stack);
+			exit(EXIT_FAILURE);
+		}
+	}
+	free_all(stack);
+}
